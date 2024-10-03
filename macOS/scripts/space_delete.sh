996b3e2f483647da2d864276626d6170afb6a61d
Santiago Pajuelo <santiago@phuru.io>
2024-10-02 20:25:49 -0500

    update

---
+++ macOS/scripts/space_delete.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "which directory would you like to check?"
+read directory
+
+find $directory -type f | while read file; do
+    if [[ "$file" = *[[:space:]]* ]]; then
+        mv "$file" `echo $file | tr ' ' '_'`
+    fi;
+done
